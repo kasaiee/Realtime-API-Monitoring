b8f001d33e3ebe1ef4feac117360744dd63a1734
mohammad babazadeh <m.babazadeh73@gmail.com>
2018-12-28 13:50:36 +0330

    init request models

--- DjangoDashboard/api/models.py
+++ DjangoDashboard/api/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class EndpointRequest(models.Model):
+    name = models.CharField(max_length=30)
+    url = models.CharField(max_length=100)
+    
