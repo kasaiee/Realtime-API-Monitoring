827fbc72621e74c90c0ec9f5a9e8194959d17316
mohammad babazadeh <m.babazadeh73@gmail.com>
2018-12-29 20:07:07 +0330

    edit header model

--- DjangoDashboard/api/models.py
+++ DjangoDashboard/api/models.py
@@ -4,7 +4,7 @@ class EndpointRequest(models.Model):
     name = models.CharField(max_length=30)
     method = models.CharField(max_length=10)
     uri = models.CharField(max_length=100)
-    header = models.Charfield(max_length=500)
+    header = models.CharField(max_length=500)
 
 
     
