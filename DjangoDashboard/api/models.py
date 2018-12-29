574cf6a165035b7c1a8cd2c5881d807ea7c2f088
mohammad babazadeh <m.babazadeh73@gmail.com>
2018-12-29 20:06:41 +0330

    add header model

--- DjangoDashboard/api/models.py
+++ DjangoDashboard/api/models.py
@@ -4,6 +4,7 @@ class EndpointRequest(models.Model):
     name = models.CharField(max_length=30)
     method = models.CharField(max_length=10)
     uri = models.CharField(max_length=100)
+    header = models.Charfield(max_length=500)
 
 
     
