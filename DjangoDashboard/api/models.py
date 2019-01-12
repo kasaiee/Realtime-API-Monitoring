85a4375e33787d248ded5e085a0e82dce7a68660
mohammad babazadeh <m.babazadeh73@gmail.com>
2019-01-12 19:02:16 +0330

    change EndpointRequest model

--- DjangoDashboard/api/models.py
+++ DjangoDashboard/api/models.py
@@ -3,22 +3,22 @@ from django.db import models
 class EndpointRequest(models.Model):
     name = models.CharField(max_length=30)
     method_choices = (
-        (0, "GET"),
-        (1, "HEAD"),
-        (2, "POST"),
-        (3, "PUT"),
-        (4, "DELETE"),
-        (5, "CONNECT"),
-        (6, "OPTIONS"),
-        (7, "TRACE"),
-        (8, "PATCH"),
+        (1, "GET"),
+        (2, "HEAD"),
+        (3, "POST"),
+        (4, "PUT"),
+        (5, "DELETE"),
+        (6, "CONNECT"),
+        (7, "OPTIONS"),
+        (8, "TRACE"),
+        (9, "PATCH"),
     )
-    method = models.CharField(max_length=10, 
+    method = models.IntegerField(max_length=10,
                         choices=method_choices, default="GET")
     uri = models.CharField(max_length=100)
-    header = models.CharField(max_length=500)
-    body = models.CharField(max_length=500)
-    description = models.CharField(max_length=500)
+    header = models.CharField(max_length=500, null=True, blank=True)
+    body = models.CharField(max_length=500, null=True, blank=True)
+    description = models.CharField(max_length=500, null=True, blank=True)
 
     
 class BaseURL(models.Model):
